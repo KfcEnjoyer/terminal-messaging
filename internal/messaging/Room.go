74f954fb366349e1f56fafa78d2fe9791b36c7ab
KfcEnjoyer <alikhan_azanbaev@mail.ru>
2025-03-10 02:57:23 -0500

    commit

--- internal/messaging/Room.go
+++ internal/messaging/Room.go
@@ -1,9 +1,8 @@
 package messaging
 
 type Room struct {
-	Name  string
-	Owner string
-	Users []string
-	// Add a field for the user performing the action (if applicable)
-	UserJoining string // this field would only be used for join requests
+	Name        string
+	Owner       string
+	Users       []string
+	UserJoining string
 }
